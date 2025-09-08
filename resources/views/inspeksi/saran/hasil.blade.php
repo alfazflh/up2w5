dde9be2da9b5cecf4bd8600ae7a0468aefdf8292
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-08 13:48:34 +0700

    new commit

--- resources/views/inspeksi/saran/hasil.blade.php
+++ resources/views/inspeksi/saran/hasil.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Dashboard Kritik dan Saran K3L & Keamanan UP2WVI</title>
+    <title>Dashboard Kritik dan Saran K3L & Keamanan UP2WV</title>
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/97/Logo_PLN.png" type="image/png" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
@@ -48,7 +48,7 @@
             KRITIK DAN SARAN K3L & KEAMANAN
             </h1>
             <h2 class="text-xs sm:text-sm text-white font-semibold">
-            PLN PUSHARLIS UP2W VI
+            PLN PUSHARLIS UP2W V
             </h2>
         </div>
 
@@ -58,7 +58,7 @@
                 KRITIK DAN SARAN K3L & KEAMANAN
             </h1>
             <h2 class="text-sm sm:text-base md:text-xl text-white font-semibold mt-1">
-            PLN PUSHARLIS UP2W VI
+            PLN PUSHARLIS UP2W V
             </h2>
         </div>
 
