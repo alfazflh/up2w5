f8c891b4b98668268e9eb7fc689b68154d10f9e4
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-10 11:01:11 +0700

    new commit

--- resources/views/inspeksi/apat/index.blade.php
+++ resources/views/inspeksi/apat/index.blade.php
@@ -105,7 +105,7 @@
         <h2 class="text-xl font-bold mb-4">Tambah APAT Baru</h2>
         <form action="{{ route('apat.store') }}" method="POST">
             @csrf
-            <div class="mb-3"><label class="block font-semibold">ID APAT</label><input type="text" name="id_apat" required class="w-full px-3 py-2 border rounded" placeholder="cth: A1.01"></div>
+            <div class="mb-3"><label class="block font-semibold">ID APAT</label><input type="text" name="id_apat" required class="w-full px-3 py-2 border rounded" placeholder="cth: A3.01"></div>
             <div class="mb-3"><label class="block font-semibold">Lokasi</label><input type="text" name="lokasi" required class="w-full px-3 py-2 border rounded"></div>
             
 
