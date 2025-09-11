07963a6f3541b45631b470f8b7a9fe95043ed3c4
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-11 09:32:57 +0700

    new commit

---
+++ app/Models/Apab.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Apab extends Model
+{
+    protected $table = 'apars';
+    protected $fillable = [
+        'id_apab',
+        'lokasi',
+        'isi_apab',
+        'kapasitas',
+        'masa_berlaku',
+        'catatan',
+        'kesimpulan',
+    ];
+}
