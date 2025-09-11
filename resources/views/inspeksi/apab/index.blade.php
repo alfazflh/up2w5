1af1d39f6783af333b8e958cb7efe2384d4d9180
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-11 10:38:28 +0700

    new commit

--- resources/views/inspeksi/apab/index.blade.php
+++ resources/views/inspeksi/apab/index.blade.php
@@ -40,7 +40,7 @@
 
         <div class="flex flex-col text-center md:hidden">
             <h1 class="font-bold text-white leading-tight text-sm sm:text-lg">
-            MONITORING ALAT PEMADAM API RINGAN
+            MONITORING ALAT PEMADAM API BERAT
             </h1>
             <h2 class="text-xs sm:text-sm text-white font-semibold">
             PLN PUSHARLIS UP2W V
@@ -50,7 +50,7 @@
         <div class="absolute inset-x-0 text-center hidden md:block">
             <h1 class="font-bold text-white leading-tight"
                 style="font-size: clamp(1rem, 2vw + 0.5rem, 2rem);">
-                MONITORING ALAT PEMADAM API RINGAN
+                MONITORING ALAT PEMADAM API BERAT
             </h1>
             <h2 class="text-sm sm:text-base md:text-xl text-white font-semibold mt-1">
             PLN PUSHARLIS UP2W V
